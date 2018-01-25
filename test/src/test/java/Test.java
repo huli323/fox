4e376dec8a0606f02201c6cd5082ace349e149e6
fox <1037813603@qq.com>
2018-01-25 17:37:49 +0800

    no message

--- test/src/test/java/Test.java
+++ test/src/test/java/Test.java
@@ -4,14 +4,33 @@ import java.text.ParseException;
 public class Test {
     public static void main(String[] args) throws IOException, IllegalAccessException, ParseException, InterruptedException {
 
-        System.out.println("1|1|1||".split("\\|", 5).length);
-
-        for (String s :
-                "1|1|1||".split("\\|", 5)) {
-            System.out.println(s);
-        }
 
     }
 
 }
+
+class A{
+    A(){
+        System.out.println("class A init");
+    }
+    static {
+        System.out.println("class A static block");
+    }
+    {
+        System.out.println("class A block");
+    }
+}
+class B extends A{
+    B(){
+        System.out.println("class B init");
+
+    }
+    static {
+        System.out.println("class B static block");
+    }
+    {
+        System.out.println("class B block");
+    }
+}
+
 
